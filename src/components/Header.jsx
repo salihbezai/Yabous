3a511384dd490875595a232e74ae069bfc9f9f51
salihbezai <salihbezai98@gmail.com>
2025-03-18 23:02:15 +0100

    making header responsive on small devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,18 +8,21 @@ const Header = () => {
   return (
     <header className="">
       <div className="bg-color-bg-1 text-white">
-        <div className="flex justify-center relative px-1 py-3 store-container ">
-          <div className="flex">
-            <p className="text-base">
+        <div className="flex  justify-center items-center  relative px-1 py-3 store-container ">
+          <div className="flex-1 text-center mr-2">
+            <p className="text-base sm:text-base md:text-lg lg:text-lg xl:text-xl">
               Summer Sale For All Swim Suits And Free Express Delivery - OFF
               50%!
-              <a className="text-base underline ml-2" href="/">
+              <a
+                className="text-base underline ml-2 sm:text-base md:text-lg lg:text-lg xl:text-xl"
+                href="/"
+              >
                 ShopNow
               </a>
             </p>
           </div>
-          <div className="absolute right-4 flex justify-center cursor-pointer ">
-            <select>
+          <div className="flex  cursor-pointer items-center ">
+            <select className="cursor-pointer">
               <option
                 value="en"
                 className="text-color-1 cursor-pointer"
@@ -37,43 +40,51 @@ const Header = () => {
           </div>
         </div>
       </div>
-      <div className="px-2 py-5 store-container flex items-center justify-between">
-        <div>
-          <Link to={"/"}>
-            <h1 className="font-extrabold text-lg">Yabous</h1>
-          </Link>
+      <div
+        className="px-2 py-5 store-container flex items-center justify-between 
+      flex-col sm:flex-col gap-2 md:flex-row"
+      >
+        <div className="flex items-center justify-between w-full">
+          <div className="mr-3">
+            <Link to={"/"}>
+              <h1 className="font-extrabold xs:text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+                Yabous
+              </h1>
+            </Link>
+          </div>
+          <nav className=" ml-2">
+            <ul className="flex gap-3">
+              <li className="text-base sm:text-base md:text-lg lg:text-lg xl:text-xl">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="text-base sm:text-base md:text-lg lg:text-lg xl:text-xl">
+                <Link to="/">Contact</Link>
+              </li>
+              <li className="text-base sm:text-base md:text-lg lg:text-lg xl:text-xl">
+                <Link to="/">About</Link>
+              </li>
+              <li className="text-base sm:text-base md:text-lg lg:text-lg xl:text-xl">
+                <Link to="/">Signup</Link>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <nav>
-          <ul className="flex gap-3">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/">Contact</Link>
-            </li>
-            <li>
-              <Link to="/">About</Link>
-            </li>
-            <li>
-              <Link to="/">Signup</Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="flex items-center gap-3">
-          <div className="flex items-center relative px-1 py-1 bg-[#F5F5F5] rounded">
+        <div className="flex items-center gap-3 w-full sm:w-full md:w-auto">
+          <div className="flex items-center relative px-1 py-1 bg-[#F5F5F5] rounded w-full sm:w-full md:w-auto">
             <input
               type="text"
               placeholder="What are you looking for?"
-              className="text-black border-none outline-none bg-[#F5F5F5] px-1 py-1 rounded text-sm mr-4"
+              className="text-black sm:text-sm md:text-base lg:text-base xl:text-lg
+               border-none outline-none bg-[#F5F5F5] px-1 py-1 rounded text-sm mr-4"
             />
-            <MagnifyingGlassIcon className="icon-style text-black absolute right-0" />
+            <MagnifyingGlassIcon className="icon-style text-black absolute right-0 w-6 h-6  sm:w-5 sm:h-5 md:w-6 md:h-6 lg:w-6 lg:h-6 xl:w-7 xl:h-7" />
           </div>
           <div className="flex items-center gap-2">
             <div>
-              <HeartIcon className="icon-style text-black" />
+              <HeartIcon className="icon-style text-black w-6 h-6  sm:w-5 sm:h-5 md:w-6 md:h-6 lg:w-6 lg:h-6 xl:w-7 xl:h-7" />
             </div>
             <div>
-              <ShoppingCartIcon className="icon-style text-black" />
+              <ShoppingCartIcon className="icon-style text-black w-6 h-6  sm:w-5 sm:h-5 md:w-6 md:h-6 lg:w-6 lg:h-6 xl:w-7 xl:h-7" />
             </div>
           </div>
         </div>
