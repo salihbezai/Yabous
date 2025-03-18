057798f747c703cd2526c6e992fab5cc1cef20f2
salihbezai <salihbezai98@gmail.com>
2025-03-18 01:04:36 +0100

    adding nav in the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
 import Header from "./components/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header />
+      <Router>
+        <Header />
+        <Routes></Routes>
+      </Router>
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
-import { ChevronDownIcon } from "@heroicons/react/16/solid";
-import { Select } from "@chakra-ui/react";
+import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
+
+import { HeartIcon, ShoppingCartIcon } from "@heroicons/react/24/outline";
+
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header>
+    <header className="">
       <div className="bg-color-bg-1 text-white">
-        <div className="flex justify-center relative px-1 py-3">
+        <div className="flex justify-center relative px-1 py-3 store-container ">
           <div className="flex">
             <p className="text-base">
               Summer Sale For All Swim Suits And Free Express Delivery - OFF
@@ -31,6 +34,47 @@ const Header = () => {
                 French
               </option>
             </select>
+          </div>
+        </div>
+      </div>
+      <div className="px-2 py-5 store-container flex items-center justify-between">
+        <div>
+          <Link to={"/"}>
+            <h1 className="font-extrabold text-lg">Yabous</h1>
+          </Link>
+        </div>
+        <nav>
+          <ul className="flex gap-3">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/">Contact</Link>
+            </li>
+            <li>
+              <Link to="/">About</Link>
+            </li>
+            <li>
+              <Link to="/">Signup</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="flex items-center gap-3">
+          <div className="flex items-center relative px-1 py-1 bg-[#F5F5F5] rounded">
+            <input
+              type="text"
+              placeholder="What are you looking for?"
+              className="text-black border-none outline-none bg-[#F5F5F5] px-1 py-1 rounded text-sm mr-4"
+            />
+            <MagnifyingGlassIcon className="icon-style text-black absolute right-0" />
+          </div>
+          <div className="flex items-center gap-2">
+            <div>
+              <HeartIcon className="icon-style text-black" />
+            </div>
+            <div>
+              <ShoppingCartIcon className="icon-style text-black" />
+            </div>
           </div>
         </div>
       </div>
