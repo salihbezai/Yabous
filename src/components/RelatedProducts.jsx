36f0bf14280c43e9dc796c11c98e34d46b88dda4
salihbezai <salihbezai98@gmail.com>
2025-04-09 09:14:42 +0200

    finishing the product details component

---
+++ src/components/RelatedProducts.jsx
@@ -0,0 +1,151 @@
+import { MdOutlineRemoveRedEye } from "react-icons/md";
+import SectionTitle from "./SectionTitle";
+import { HeartIcon } from "@heroicons/react/24/outline";
+import StarRatings from "react-star-ratings";
+
+const RelatedProducts = () => {
+  const products = [
+    {
+      id: 1,
+      name: "Breed Dry Dog Food",
+      price: "$100",
+      img: "/images/71RdoeXxtrL 1.png",
+      isNew: false,
+    },
+    {
+      id: 2,
+      name: "GCANON EOS DSLR Camera",
+      price: "$360",
+      img: "/images/eos-250d-03-500x500 1.png",
+      isNew: false,
+    },
+    {
+      id: 3,
+      name: "ideapad-gaming-3i-01-500x500 1",
+      price: "$700",
+      img: "/images/ideapad-gaming-3i-01-500x500 1.png",
+      isNew: false,
+    },
+    {
+      id: 4,
+      name: "Curology Product Set ",
+      price: "$500",
+      img: "/images/curology-j7pKVQrTUsM-unsplash 1.png",
+      isNew: false,
+    },
+    {
+      id: 5,
+      name: "Kids Electric Car",
+      price: "$960",
+      img: "/images/New-Mercedes-Benz-Gtr-Licensed-Ride-on-Car-Kids-Electric-Toy-Car 1.png",
+      isNew: true,
+    },
+    {
+      id: 6,
+      name: "Jr. Zoom Soccer Cleats",
+      price: "$1150",
+      img: "/images/Copa_Sense 1.png",
+      isNew: false,
+    },
+    {
+      id: 7,
+      name: "GP11 Shooter USB Gamepad",
+      price: "$660",
+      img: "/images/GP11_PRD3 1.png",
+      isNew: true,
+    },
+    {
+      id: 8,
+      name: "Quilted Satin Jacket",
+      price: "$660",
+      img: "/images/698717_Z8A1X_3475_001_100_0000_Light-Reversible-quilted-satin-jacket 1.png",
+      isNew: false,
+    },
+  ];
+
+  const changeRating = (newRating) => {
+    console.log(newRating);
+  };
+
+  return (
+    <div className="py-5 mt-15">
+      <div className="flex items-center gap-3 py-1">
+        <div className="w-4 h-8 secondaryColorBg rounded"></div>
+        <div>
+          <h4 className="secondaryColorText font-bold">Related Items</h4>
+        </div>
+      </div>
+      <div>
+        <div
+          className="py-10 place-items-center grid grid-cols-1 sm:grid-cols-2
+  md:grid-cols-3 lg:grid-cols-4 w-full gap-8"
+        >
+          {products.map((product) => (
+            <div key={product.id} className="relative">
+              <div
+                className="relative flex items-center flex-col justify-center   shadow-md 
+            rounded-md bg-color-bg-3 cursor-pointer min-w-[300px] max-w-[310px] min-h-[320px] max-h-[420px] sm:min-w-[300px] sm:max-w-[320px] sm:min-h-[350px] sm:max-h-[420px]
+             md:min-w-[200px] md:max-w-[230px] md:min-h-[250px] md:max-h-[280px]"
+              >
+                <img
+                  src={product.img}
+                  alt={product.name}
+                  className=" w-70 h-70 sm:w-70 sm:h-70  md:w-40 md:h-40  lg:w-40 lg:h-40 object-contain hover:scale-120 transition-all  ease-in-out"
+                />
+
+                {product.isNew ? (
+                  <span className="bg-color-bg-4 text-white px-2 rounded  text-sm absolute top-3 left-1">
+                    {"New"}
+                  </span>
+                ) : null}
+                <div className="absolute top-3  right-0 flex flex-col gap-3">
+                  <div className="bg-white px-1 py-1 rounded-full">
+                    <HeartIcon className="icon-style text-black" />
+                  </div>
+                  <div className="bg-white px-1 py-1 rounded-full">
+                    <MdOutlineRemoveRedEye className="icon-style text-black" />
+                  </div>
+                </div>
+              </div>
+              <div className="w-[250px] py-3">
+                <p
+                  className="font-bold text-base text-black
+                   sm:text-sm md:text-base lg:text-base  xl:text-base"
+                >
+                  {product.name}
+                </p>
+                <div className="flex items-center gap-4">
+                  <p className="secondaryColorText font-bold">
+                    {product.price}
+                  </p>
+                  <div className="flex items-center gap-3 text-sm">
+                    <StarRatings
+                      rating={5}
+                      starRatedColor="#FFAD33"
+                      changeRating={changeRating}
+                      numberOfStars={5}
+                      name="rating"
+                      starDimension="14px"
+                      starSpacing="5px"
+                    />
+                    <p className="greyColorText font-bold">(88)</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="flex justify-center items-center w-full">
+          <button
+            className="button-theme mt-3 sm:mt-3 md:mt-0 lg:mt-0 xl:mt-0 shadow-gray-400
+               text-slate-100 bg-[#DB4444] rounded py-1.5"
+          >
+            View All Products
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RelatedProducts;
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,3 +1,6 @@
+import RelatedProducts from "../components/RelatedProducts";
+import { HeartIcon } from "@heroicons/react/24/outline";
+
 import {
   FormControl,
   FormControlLabel,
@@ -6,6 +9,9 @@ import {
   RadioGroup,
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
+import { FaShippingFast } from "react-icons/fa";
+import { GrReturn } from "react-icons/gr";
+import { IoReturnUpBack } from "react-icons/io5";
 import { useParams } from "react-router-dom";
 import StarRatings from "react-star-ratings";
 
@@ -37,132 +43,203 @@ const ProductDetails = () => {
         <div>/</div>
         <div className="text-gray-600 font-bold">Havic HV G-92 Gamepad</div>
       </div>
-      <div className="flex gap-5 mt-15 ">
-        <div>
-          <div className="flex-col">
-            <div className="bg-[#F5F5F5] px-2 py-2">
-              <img src="/images/controller.png" alt="" className="w-20 h-20" />
-            </div>
-            <div className="bg-[#F5F5F5] px-2 py-2">
-              <img src="/images/controller.png" alt="" className="w-20 h-20" />
-            </div>
-            <div className="bg-[#F5F5F5] px-2 py-2">
-              <img src="/images/controller.png" alt="" className="w-20 h-20" />
-            </div>
-            <div className="bg-[#F5F5F5] px-2 py-2">
-              <img src="/images/controller.png" alt="" className="w-20 h-20" />
-            </div>
-          </div>
-        </div>
-        <div className="flex  gap-5">
-          <div className="bg-[#F5F5F5] w-full px-5 py-5">
-            <img src="/images/controller.png" alt="" className="h-100 w-100" />
-          </div>
-          <div className="w-full flex flex-col">
-            <h1>Havic HV G-92 Gamepad</h1>
-            <div className="flex items-center gap-3 text-sm ">
-              <StarRatings
-                rating={5}
-                starRatedColor="#FFAD33"
-                numberOfStars={5}
-                name="rating"
-                starDimension="14px"
-                starSpacing="5px"
+      <div className="flex gap-5 mt-15 flex-col lg:flex-row py-10 ">
+        <div className="flex gap-5 w-full h-full">
+          <div className=" flex flex-col gap-4 h-full">
+            <div className="bg-[#F5F5F5] px-7 py-3 ">
+              <img
+                src="/images/controller.png"
+                alt=""
+                className="object-contain w-30 h-30"
               />
             </div>
-            <div className="mb-8">
-              <p className="greyColorText font-bold flex items-center">
-                (150 Reviews) |{" "}
-                <span className="text-[#00FF66]">{"In Stock"}</span>
-              </p>
-              <h1 className="mt-5 mb-5">$192.00</h1>
-              <p>
-                PlayStation 5 Controller Skin High quality vinyl with air
-                channel adhesive for easy bubble free install & mess free
-                removal Pressure sensitive.
-              </p>
+            <div className="bg-[#F5F5F5] px-7 py-3">
+              <img
+                src="/images/controller.png"
+                alt=""
+                className="object-contain w-30 h-30"
+              />
             </div>
-            <hr className="font-light opacity-50" />
-            <div>
-              <div>
-                <div className="flex items-center gap-5">
-                  <div>Colours:</div>
+            <div className="bg-[#F5F5F5] px-7 py-3">
+              <img
+                src="/images/controller.png"
+                alt=""
+                className="object-contain w-30 h-30"
+              />
+            </div>
+            <div className="bg-[#F5F5F5] px-7 py-3">
+              <img
+                src="/images/controller.png"
+                alt=""
+                className="object-contain w-30 h-30"
+              />
+            </div>
+          </div>
 
-                  <FormControl>
-                    <RadioGroup row defaultValue="blue" name="color-selection">
-                      {colors.map((item) => (
-                        <FormControlLabel
-                          key={item.value}
-                          value={item.value}
-                          control={
-                            <Radio
-                              sx={{
-                                color: item.colorCode,
-                                "&.Mui-checked": {
-                                  color: item.colorCode,
-                                },
-                              }}
-                            />
-                          }
-                          label=""
+          <div className="bg-[#F5F5F5] w-full px-5 py-5 flex items-center justify-center">
+            <img
+              src="/images/controller.png"
+              alt=""
+              className="h-[400px] w-[400px]  "
+            />
+          </div>
+        </div>
+
+        <div className="w-full flex flex-col ">
+          <h1 className="text-3xl">Havic HV G-92 Gamepad</h1>
+
+          <div className="mb-8">
+            <div className="greyColorText font-bold flex items-center space-x-3 mt-2">
+              <div className="flex items-center gap-3 text-sm ">
+                <StarRatings
+                  rating={5}
+                  starRatedColor="#FFAD33"
+                  numberOfStars={5}
+                  name="rating"
+                  starDimension="14px"
+                  starSpacing="5px"
+                />
+              </div>
+              <span>(150 Reviews) | </span>
+              <span className="text-[#00FF66]">{"In Stock"}</span>
+            </div>
+            <h1 className="mt-4 mb-5 text-xl md:text-2xl">$192.00</h1>
+            <p className="text-sm md:text-base ">
+              PlayStation 5 Controller Skin High quality vinyl with air channel
+              adhesive for easy bubble free install & mess free removal Pressure
+              sensitive.
+            </p>
+          </div>
+          <hr className="font-light opacity-50" />
+          <div className="mt-5">
+            <div className="flex items-center gap-5 mb-2">
+              <div className="font-bold">Colours:</div>
+
+              <FormControl>
+                <RadioGroup row defaultValue="blue" name="color-selection">
+                  {colors.map((item) => (
+                    <FormControlLabel
+                      key={item.value}
+                      value={item.value}
+                      control={
+                        <Radio
                           sx={{
-                            ".MuiFormControlLabel-label": {
-                              display: "none",
+                            color: item.colorCode,
+                            "&.Mui-checked": {
+                              color: item.colorCode,
                             },
                           }}
                         />
-                      ))}
-                    </RadioGroup>
-                  </FormControl>
+                      }
+                      label=""
+                      sx={{
+                        ".MuiFormControlLabel-label": {
+                          display: "none",
+                        },
+                      }}
+                    />
+                  ))}
+                </RadioGroup>
+              </FormControl>
+            </div>
+            <div className="mb-2">
+              <FormControl>
+                <div className="font-bold">Size:</div>
+                <RadioGroup
+                  row
+                  defaultValue="M"
+                  name="size-selection"
+                  sx={{ gap: 1 }}
+                  onChange={handleSizeChange}
+                >
+                  {sizes.map((size) => (
+                    <FormControlLabel
+                      key={size}
+                      value={size}
+                      control={<Radio sx={{ display: "none" }} />}
+                      label={size}
+                      sx={{
+                        m: 0,
+                        px: 2,
+                        py: 1,
+                        borderRadius: "6px",
+                        border: "1px solid #ccc",
+                        cursor: "pointer",
+                        color: selectedSize === size ? "white" : "black",
+                        backgroundColor:
+                          selectedSize === size ? "#DB4444" : "white",
+                        transition: "0.2s",
+                        "&.Mui-checked, &.MuiFormControlLabel-root.Mui-checked":
+                          {
+                            backgroundColor: "#DB4444",
+                            color: "white",
+                            borderColor: "#e74c3c",
+                          },
+                        "&:hover": {
+                          backgroundColor: "#f4f4f4",
+                        },
+                        "& .MuiTypography-root": {
+                          fontWeight: "bold",
+                        },
+                      }}
+                    />
+                  ))}
+                </RadioGroup>
+              </FormControl>
+            </div>
+
+            <div className="flex  gap-5 mt-5 flex-col lg:flex-row">
+              <div className="border-1 flex items-center w-fit h-fit">
+                <button
+                  className="button-theme font-bold 
+                  hover:bg-[#DB4444] hover:text-white border-1 rounded-none"
+                >
+                  -
+                </button>
+                <button className="button-theme font-bold hover:bg-[#DB4444] hover:text-white border-1 rounded-none">
+                  2
+                </button>
+                <button className="button-theme font-bold hover:bg-[#DB4444] hover:text-white border-1 rounded-none">
+                  +
+                </button>
+              </div>
+              <div className="flex items-center gap-5">
+                <button className="button-theme  bg-[#DB4444] text-white">
+                  Buy Now
+                </button>
+                <button className="button-theme w-[5px]flex ">
+                  <HeartIcon className="icon-style text-black" />
+                </button>
+              </div>
+            </div>
+            <div className="border-1 mt-10">
+              <div className="flex items-center gap-3 px-3 py-3">
+                <div className="flex items-center">
+                  <FaShippingFast className="icon-style text-black w-7 h-7" />
                 </div>
-                <FormControl>
-                  <FormLabel>Size:</FormLabel>
-                  <RadioGroup
-                    row
-                    defaultValue="M"
-                    name="size-selection"
-                    sx={{ gap: 1 }}
-                    onChange={handleSizeChange}
-                  >
-                    {sizes.map((size) => (
-                      <FormControlLabel
-                        key={size}
-                        value={size}
-                        control={<Radio sx={{ display: "none" }} />}
-                        label={size}
-                        sx={{
-                          m: 0,
-                          px: 2,
-                          py: 1,
-                          borderRadius: "6px",
-                          border: "1px solid #ccc",
-                          cursor: "pointer",
-                          color: selectedSize === size ? "white" : "black",
-                          backgroundColor:
-                            selectedSize === size ? "#DB4444" : "white",
-                          transition: "0.2s",
-                          "&.Mui-checked, &.MuiFormControlLabel-root.Mui-checked":
-                            {
-                              backgroundColor: "#DB4444",
-                              color: "white",
-                              borderColor: "#e74c3c",
-                            },
-                          "&:hover": {
-                            backgroundColor: "#f4f4f4",
-                          },
-                          "& .MuiTypography-root": {
-                            fontWeight: "bold",
-                          },
-                        }}
-                      />
-                    ))}
-                  </RadioGroup>
-                </FormControl>
+                <div>
+                  <h2 className="font-bold text-base">Free Delivery</h2>
+                  <p className="text-sm">
+                    Enter your postal code for Delivery Availability
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-center gap-3 px-3 py-3 border-t-1">
+                <div className="flex items-center">
+                  <GrReturn className="icon-style text-black w-7 h-7" />
+                </div>
+                <div>
+                  <h2 className="font-bold text-base">Return Delivery</h2>
+                  <p className="text-sm">
+                    Free 30 Days Delivery Returns. Details
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <RelatedProducts />
     </div>
   );
 };
